f358728957eff7ac53e76233ddfb7287b2b1c5a5
Jaykob Walson <jwalson@pugetsound.edu>
2021-09-28 16:46:55 -0700

    Got help to add a 6 letter word for the player to guess 3 to 6 subwords out of.

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 let random = Math.floor(Math.random()*dictionary.length);
 let word = dictionary [random];
-console.log("The word is " + word);
+if(word.length == 6){//6 character limit for the word
+console.log("The letters of the words are the following:");
+    for(let i = 0; i < word.length; i++){//Prints out the words in random order.
+        let position = Math.floor(Math.random()*word.length);
+        console.log(word[position]);
+    }
+console.log("Enter all possible 3-6 character words");
+    for(let w in dictionary){
+       if(w.length >= 3 && w.length <= 6){
+           for(let i = 0; i < word.length; i++){
+            if(w[i]){
+
+            }
+           }
+
+       }
+    }
+}
